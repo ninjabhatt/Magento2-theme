06f1ae88b2f64c289819eae98c9518f0e01150e1
ninjabhatt <99594412+ninjabhatt@users.noreply.github.com>
2022-02-14 17:16:25 +0530

    Add files via upload

---
+++ frontend/Custom/childtheme/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Custom/childtheme', __DIR__);
---
+++ frontend/Custom/childtheme/web/requirejs-config.js
@@ -0,0 +1,11 @@
+var config = {
+    paths: {
+        slick: 'js/slick'
+    },
+    shim: {
+        slick: {
+            deps: ['jquery']
+        }
+    },
+    deps:['js/main'] 
+};
